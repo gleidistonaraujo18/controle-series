d14d7737e02e83d1872b0e397f439cedd729bcc9
gleidistonaraujo18 <gleidistonaraujo18@gmail.com>
2023-07-15 17:23:37 -0300

    Conclusão do terceiro curso

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,28 +1,48 @@
 <!doctype html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - Controle de Séries</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
-<div class="container">
-    <h1>{{ $title }}</h1>
 
-    @if ($errors->any())
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('series.index') }}">Home</a>
+
+            @auth
+            <a class="btn btn-danger" href="{{ route('logout') }}">Sair</a>
+            @endauth
+
+        </div>
+    </nav>
+    <div class="container">
+        <h1>{{ $title }}</h1>
+
+
+        @isset($mensagemSucesso)
+        <div class="alert alert-success">
+            {{ $mensagemSucesso }}
+        </div>
+        @endisset
+
+
+        @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                <li>{{ $error }}</li>
                 @endforeach
             </ul>
         </div>
-    @endif
+        @endif
 
-    {{ $slot }}
-</div>
+        {{ $slot }}
+    </div>
 </body>
+
 </html>
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,19 +1,13 @@
-<x-layout title="Séries">
-    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
-
-    @isset($mensagemSucesso)
-    <div class="alert alert-success">
-        {{ $mensagemSucesso }}
-    </div>
-    @endisset
+<x-layout title="Séries" :mensagem-sucesso="$mensagemSucesso">
+@auth<a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>@endauth
 
     <ul class="list-group">
         @foreach ($series as $serie)
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="{{ route('seasons.index', $serie->id) }}">
+        @auth<a href="{{ route('seasons.index', $serie->id) }}">@endauth
                 {{ $serie->nome }}
-            </a>
-
+                @auth</a>@endauth
+            @auth
             <span class="d-flex">
                 <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
                     E
@@ -27,6 +21,8 @@
                     </button>
                 </form>
             </span>
+
+            @endauth
         </li>
         @endforeach
     </ul>
